ce70c21594f91204edb79ecd6c6398d2cc65b078
Mahaz noor <mahaznoori@gmail.com>
2026-01-29 20:06:16 +0500

    task4

---
+++ modules/modules/mypackage/main.py
@@ -0,0 +1,5 @@
+from mypackage import multiply, divide, log_message
+
+print("Multiply:", multiply(4, 5))
+print("Divide:", divide(10, 2))
+log_message("Application started successfully")
---
+++ modules/modules/mypackage/mypackage/mypackage/logger.py
@@ -0,0 +1,6 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+def log_message(msg):
+    logging.info(msg)
